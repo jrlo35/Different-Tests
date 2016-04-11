1212423cddb6ee2b6cf1ca6fc32631e23ac6d10c
Jonathon Lo <jonathon.lo35@gmail.com>
2016-04-10 23:36:08 -0700

    create server for integration test

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,34 @@
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
-  it('todo.setup receives an array of todos when todo.init is called', function () {
+
+
+  before(function() {
+  	server = sinon.fakeServer.create();
+
+  	setupStub = sinon.stub(todo,'setup');
+
+  	JSONresponse = {
+  		todos: [
+  		{
+
+  			name: 'run tests',
+  			done: true
+  		},
+  		{
+  			name: 'more tests',
+  			done: true
+  		},
+  		{
+  			name: 'more more tests',
+  			done: true
+  		}
+  		]
+  	};
+
+  	server.respondWith('GET', 'http://localhost:3000/todos',
+  		[200,{'Content-Type': 'application/json'}, JSON.stringify(JSONresponse)]);
   });
+
+  
 });
