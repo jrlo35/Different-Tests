f8872d65b92c0cd295e965c578713c7a3f92dc61
Jonathon Lo <jonathon.lo35@gmail.com>
2016-04-19 23:21:29 -0700

    add casper test that adds and removes todo items

--- test/e2e/todoFunctionality.js
+++ test/e2e/todoFunctionality.js
@@ -10,4 +10,23 @@ casper.test.begin('App is setup correctly', 2, function suite(test) {
   });
 });
 
+//Test that adds and removes todo items
+casper.test.begin('Adds and removes todo items',3, function suite(test){
+	casper.start('http://localhost:3000/', function (){
+		this.fill('.todo-form', {
+			todo: 'Write tests'
+		}, true);
 
+		//Asserts that an element matching the provided selector expression exists in remote DOM environment
+		test.assertExists('.todo-list .todo-item', 'Added todo should exists');
+
+		//Asserts that a given form field has the provided value with input name or selector expression
+		test.assertField({type: 'css', path: '.todo-list' '.todo-item' '.todo-input'}, 'Write tests', 'Todo list should have added todo');
+
+		//Performs a click on element matching the provided selector expression
+		this.click('.todo-remove');
+
+		//Asserts that an element matching the provided selector expression doesn't exists within the remote DOM environment
+		test.assertDoesntExist('.todo-list .todo-item', 'Todo should not exist after removed');
+	});
+})
